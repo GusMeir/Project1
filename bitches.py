02a25a49da11933979296f166025ec52966d1379
MrQQish <patrick.l.miller1299@gmail.com>
2022-02-12 15:02:26 +1100

    ask for number infintely

--- bitches.py
+++ bitches.py
@@ -1,24 +1,40 @@
-# number = input("pick a number: ") 
-# print("this is your number,") 
-# print(number) 
+########################## MODULES GO AT THE TOP
+# if they are not imported before they are used it will crach this is because python is a script and interpreted line by line 
+import pandas as pd
+import numpy as np 
 
-
+#### CONTANTS
 affirmitive_responses = ["yes","Yes","y","Y","Okay","okay","Fine","fine"]
-decision = input("Hi Angus, my name is Karen. Would you like to pick a number for me? Type yes or no \n")
 
-if decision in affirmitive_responses:
-    print("pick a number") 
-   
-    newnumber = input()
-    # print(int(newnumber)) 
-    try:
-        newnumber = int(newnumber) 
-    except: 
-        print("You didn't put in a number faggot") 
-    print(newnumber , "is a very nice number well done!") 
-    print("Now can I add my number to yours?")
-    decision = input() 
+
+###################################### MAIN
+if __name__ == "__main__":
+    
+    name = input("I am Karen, who am I speaking to?\n")
+
+    decision = input("Hi " + name + ". Would you like to pick a number for me? Type yes or no \n")
+
     if decision in affirmitive_responses:
-        print("666") 
-else: # this will run if they dont say literally yes
-    pass
+    
+        newnumber = input("pick a number\n")
+
+       # make sure the value can be turned into an integer otherwise tell them they are stupid 
+       
+        wrong_attempts = 0  
+        while type(newnumber) is str:
+            try:
+                newnumber = int(newnumber) 
+            except: 
+                print("You didn't put in a number faggot") 
+                wrong_attempts += 1 
+                annoy = wrong_attempts*"new "
+                newnumber = input("pick a " + annoy + "number\n")
+
+
+        decision = input("Now can I add my number to yours?") 
+
+        if decision in affirmitive_responses:
+            print("666") 
+
+    else: # this will run if they dont say literally yes
+        print("Well I'd like to talk to yoour manager, you fat cunt. I'm angry, go kill yourself now please")
--- hoes.py
+++ hoes.py
@@ -1,3 +1,6 @@
+import .bitches
+
+
 print("hoes mad ")
 print("they can stay mad    ")
 tyfcituc = 1 
