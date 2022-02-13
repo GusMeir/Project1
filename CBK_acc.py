5b3d40d93f31e3c676ac2c6ba361f5f585b27066
GusMeir <eric.guymer14@gmail.com>
2022-02-13 23:01:41 +1100

    implemented point of intercept

--- CBK_acc.py
+++ CBK_acc.py
@@ -14,15 +14,27 @@ i = 26 #initial cost
 s = 20 #selling price 
 c = 10 #cost, material and labour 
 
+xi = (i-0) / (s-c) # xi, yi; variables for plotting point of intercept
+yi = c * xi + i 
 
 x = np.linspace(0,30,30)
 y = s*x
-z = c*x+i
+z = c*x+i       # z = cost line 
+
 plt.plot(x, y, z, '-r') 
 plt.title('CBK shirts, Income/Cost relationship')
 
 plt.xlabel('No. shirts made/sold')
 plt.ylabel('Income/Cost')
 
-plt.grid()
-plt.show() 
+print('(xi,yi)',xi,yi) #prints number of products needed to sell to break even (first value I want to round up)
+
+plt.scatter(xi,yi, color='black' ) #plots black dot on grid for POI
+plt.savefig("two_straight_lines_intersection_point_02.png", bbox_inches='tight') 
+
+plt.grid() #generates grid? 
+plt.show() #shows image? 
+
+
+
+
