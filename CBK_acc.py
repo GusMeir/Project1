9996e74a8068f5108cde2dca13dd1a2721b01fb0
GusMeir <eric.guymer14@gmail.com>
2022-02-14 12:08:36 +1100

    trying to make it inputtable

--- CBK_acc.py
+++ CBK_acc.py
@@ -10,16 +10,22 @@ shirtprice = 20
 # x_axis = "number of shirts"
 # y_axis = "Cost/Income" 
 
-i = 26 #initial cost
-s = 20 #selling price 
+# i = input("What is your initial cost? ")
+# s = input("What is your selling price? " ) 
+# c = input("How much does one product cost to make? ") 
+
+i = 1000 #initial cost
+s = 60 #selling price 
 c = 10 #cost, material and labour 
+
+x = np.linspace(0,30,30)
+y = s*x
+z = c*x+i   # z = cost line 
 
 xi = (i-0) / (s-c) # xi, yi; variables for plotting point of intercept
 yi = c * xi + i 
 
-x = np.linspace(0,30,30)
-y = s*x
-z = c*x+i       # z = cost line 
+    
 
 plt.plot(x, y, z, '-r') 
 plt.title('CBK shirts, Income/Cost relationship')
@@ -27,10 +33,10 @@ plt.title('CBK shirts, Income/Cost relationship')
 plt.xlabel('No. shirts made/sold')
 plt.ylabel('Income/Cost')
 
-print('(xi,yi)',xi,yi) #prints number of products needed to sell to break even (first value I want to round up)
+print('Product amount to break Even:',xi) #prints number of products needed to sell to break even (first value I want to round up)
 
 plt.scatter(xi,yi, color='black' ) #plots black dot on grid for POI
-plt.savefig("two_straight_lines_intersection_point_02.png", bbox_inches='tight') 
+plt.savefig("two_straight_lines_intersection_point_02.png", bbox_inches='tight') #saves model
 
 plt.grid() #generates grid? 
 plt.show() #shows image? 
